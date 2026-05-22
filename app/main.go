50a87b0b135b22bd4e1a3a20f095d66bbbb97cc9
AGX <agxnh2020@gmail.com>
2026-05-22 23:05:12 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -29,21 +30,26 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	var messageSize int32
-	binary.Read(conn, binary.BigEndian, &messageSize)
-	logger.Debug("reading the request header")
-	requestHeader, err := parseRequestHeaderv2(conn)
+	rfm := bufio.NewReader(conn)
+	request := parseRequest(rfm, logger)
+	requestBody, err := parseApiVersionsRequestBody(rfm)
+	logger.Info(fmt.Sprintf("request body: %v", requestBody))
 	if err != nil {
-		logger.Error("error while parsing the request header", "err", err.Error())
+		logger.Error("error while parsing the request body", "err", err.Error())
 	}
-	logger.Debug("DONE reading the request header")
 
-	buf := make([]byte, 0, 10)
-	buf = binary.BigEndian.AppendUint32(buf, uint32(messageSize))
-	buf = binary.BigEndian.AppendUint32(buf, uint32(requestHeader.CorrelationId))
-	buf = binary.BigEndian.AppendUint16(buf, uint16(getErrorCode(requestHeader.ApiVersion))) // error code for unsupported version
+	logger.Debug("reading the request header")
+
+	w := bufio.NewWriter(conn)
 	logger.Debug("Sending the response")
-	conn.Write(buf)
+	resp := &ApiVersionsResponse{
+		ErrorCode:      getErrorCode(request.Header.ApiVersion),
+		ApiKeys:        []ApiKey{},
+		CorrelationId:  request.Header.CorrelationId,
+		ThrottleTimeMs: 0,
+	}
+	resp.encode(w)
+	w.Flush()
 
 }
 
@@ -86,4 +92,149 @@ func getErrorCode(apiVersion int16) int16 {
 		return 0
 	}
 	return 35
+}
+
+type Request struct {
+	MessageSize int32
+	Header      *RequestHeader
+}
+
+func parseRequest(r io.Reader, logger *slog.Logger) *Request {
+	logger.Debug("BEGIN reading the request")
+	var messageSize int32
+	binary.Read(r, binary.BigEndian, &messageSize)
+
+	requestHeader, err := parseRequestHeaderv2(r)
+	if err != nil {
+		logger.Error("error while parsing the request header", "err", err.Error())
+	}
+	logger.Debug("DONE reading the request header")
+	return &Request{
+		MessageSize: messageSize,
+		Header:      requestHeader,
+	}
+}
+
+type ApiVersionsRequestBody struct {
+	ClientSoftwareName    string
+	ClientSoftwareVersion string
+	TagBuffer             byte
+}
+
+func parseApiVersionsRequestBody(r *bufio.Reader) (*ApiVersionsRequestBody, error) {
+	nameLen, err := binary.ReadUvarint(r)
+	if err != nil {
+		return nil, err
+	}
+	name := make([]byte, nameLen-1)
+	_, err = io.ReadFull(r, name)
+	if err != nil {
+		return nil, err
+	}
+
+	versionLen, err := binary.ReadUvarint(r)
+	if err != nil {
+		return nil, err
+	}
+
+	version := make([]byte, versionLen-1)
+	_, err = io.ReadFull(r, version)
+	if err != nil {
+		return nil, err
+	}
+
+	tagBuffer, err := r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	return &ApiVersionsRequestBody{
+		ClientSoftwareName:    string(name),
+		ClientSoftwareVersion: string(version),
+		TagBuffer:             tagBuffer,
+	}, nil
+}
+
+type ApiKey struct {
+	ApiKey     int16
+	MinVersion int16
+	MaxVersion int16
+}
+
+type ApiVersionsResponse struct {
+	ErrorCode      int16
+	ApiKeys        []ApiKey
+	ThrottleTimeMs int32
+	CorrelationId  int32
+}
+
+func (r *ApiVersionsResponse) encode(w *bufio.Writer) error {
+	messageSize := 4 + 2 + 1 + (6 * calcUvarintSize(uint64(len(r.ApiKeys)+1))) + 4 + 1
+	// Encode the message size
+	binary.Write(w, binary.BigEndian, uint32(messageSize))
+
+	// Encode the correlation ID
+	binary.Write(w, binary.BigEndian, uint32(r.CorrelationId))
+
+	// Encode the error code
+	err := binary.Write(w, binary.BigEndian, r.ErrorCode)
+	if err != nil {
+		return err
+	}
+
+	// Encode the number of API keys
+	writeUvarint(w, uint64(len(r.ApiKeys)+1))
+	if err != nil {
+		return err
+	}
+
+	// Encode each API key
+	for _, apiKey := range r.ApiKeys {
+		err = binary.Write(w, binary.BigEndian, apiKey.ApiKey)
+		if err != nil {
+			return err
+		}
+		err = binary.Write(w, binary.BigEndian, apiKey.MinVersion)
+		if err != nil {
+			return err
+		}
+		err = binary.Write(w, binary.BigEndian, apiKey.MaxVersion)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Encode the throttle time
+	err = binary.Write(w, binary.BigEndian, r.ThrottleTimeMs)
+	if err != nil {
+		return err
+	}
+
+	binary.Write(w, binary.BigEndian, int16(0)) // tag buffer
+
+	return nil
+
+}
+
+func writeUvarint(buf *bufio.Writer, x uint64) error {
+	for x >= 0x80 {
+		err := buf.WriteByte(byte(x) | 0x80)
+		if err != nil {
+			return err
+		}
+		x >>= 7
+	}
+	return buf.WriteByte(byte(x))
+}
+
+func calcUvarintSize(x uint64) int {
+	size := 0
+
+	for x >= 0x80 {
+		size++
+		x >>= 7
+	}
+	size++
+
+	return size
 }
